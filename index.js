29075fa02ccaf534a9c7fb77038dc52c57ea23d1
William AD <w.albertusd@gmail.com>
2018-08-19 22:27:34 +0700

    add self promotion

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const { takeCourses } = require('./scrape');
 const { user, courses, delayEachRequest } = require('./config.json');
 
 console.log("Begin gathering courses...");
+console.log("Made by William AD");
+console.log("https://www.linkedin.com/in/walbertus");
+console.log("https://github.com/walbertus");
 takeCourses(user, courses, delayEachRequest);
