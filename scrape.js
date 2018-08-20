91900c6e2d23f636760b438a3fabc68c475ae055
William AD <w.albertusd@gmail.com>
2018-08-20 17:30:47 +0700

    update scrape.js to handle personal need

--- scrape.js
+++ scrape.js
@@ -1,23 +1,23 @@
-const request = require('superagent');
+const agent = require('superagent').agent();
 const cheerio = require('cheerio');
 const Bluebird = require('bluebird');
 const _ = require('lodash');
 
-function takeCourses(user, courses, firerate = 3000, counter = 1) {
+function takeCourses(user, courses, firerate = 3000, request = agent, counter = 1,) {
     setTimeout(() => {
         console.log("Attempt #", counter);
         Bluebird.map(courses, (course) => {
-            return generateCourseRequest(user, course);
+            return generateCourseRequest(request, user, course);
         })
             .then(responses => {
                 _.each(responses, printRequestResult);
                 console.log("Attempt #", counter, "complete");
-                takeCourses(user, courses, firerate, counter + 1);
+                takeCourses(user, courses, firerate, request, counter + 1);
             })
             .catch(err => {
                 console.error(err);
                 console.log("Attempt #", counter, "error");
-                takeCourses(user, courses, firerate, counter + 1);
+                takeCourses(user, courses, firerate, request, counter + 1);
             })
     }, firerate);
 }
@@ -30,11 +30,14 @@ const printRequestResult = (response) => {
     })
 }
 
-const generateCourseRequest = (user, course) => {
-    return request.post('http://akademik3.its.ac.id/list_frs.php')
+const generateCourseRequest = (request, user, course) => {
+    const requestObject = request.post('http://akademik3.its.ac.id/list_frs.php')
         .set('Content-Type', 'application/x-www-form-urlencoded')
-        .set('Cookie', user.cookie)
-        .timeout({ response: 7000 })
+        .timeout({ response: 7000 });
+    if (user.cookie) {
+        requestObject.set('Cookie', user.cookie);
+    }
+    return requestObject
         .send({
             'nrp': user.nrp,
             'act': 'ambil',
